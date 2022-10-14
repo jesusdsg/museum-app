f28bd184e3cadeb300dfb62720838a59c9f13fb3
Jesus Salcedo <jesalceg@gmail.com>
2022-10-14 15:35:39 -0500

    Improved styles and responsive menu

--- pages/profile.js
+++ pages/profile.js
@@ -12,15 +12,22 @@ function Profile() {
     bookmarks: [],
   });
   const getProfile = async () => {
-    await axios.get("./api/profile/", {
-      withCredentials: true,
-    }).then(response => {
-    setUser(response.data);
-   })
-    .catch((error) => {
-      console.log('Error', error)
-      toast(error.response.data.error, { hideProgressBar: true, autoClose: 2000, type: 'error', position: 'bottom-right'})
-    });
+    await axios
+      .get("./api/profile/", {
+        withCredentials: true,
+      })
+      .then((response) => {
+        setUser(response.data);
+      })
+      .catch((error) => {
+        console.log("Error", error);
+        toast(error.response.data.error, {
+          hideProgressBar: true,
+          autoClose: 2000,
+          type: "error",
+          position: "bottom-right",
+        });
+      });
   };
   /* Loading the User profile based in cookies */
   useEffect(() => {
@@ -29,30 +36,30 @@ function Profile() {
 
   return (
     <Layout>
-      <div className="bg-white px-8 py-8 mt-8 mb-8 rounded-lg shadow-lg">
-        <h1 className="text-3xl font-bold">Welcome {user.name}</h1>
-        <p className="text-2xl text-slate-400">
-          {user.bookmarks == undefined || user.bookmarks.length == 0
-            ? "You don't have bookmarks yet"
-            : "This is your bookmarks list"}
-        </p>
+      <div className="px-8">
+        <div className="bg-white px-8 py-8 mt-8 mb-8 rounded-lg shadow-lg">
+          <h1 className="text-3xl font-bold">Welcome {user.name}</h1>
+          <p className="text-2xl text-slate-400">
+            {user.bookmarks == undefined || user.bookmarks.length == 0
+              ? "You don't have bookmarks yet"
+              : "This is your bookmarks list"}
+          </p>
+        </div>
+
+        <div className="lg:grid lg:grid-cols-3 gap-10 md:grid-cols-2 sm:grid-cols-1">
+          {user.bookmarks.map((bookmark) => {
+            const { id, title, image, createAt, website } = bookmark;
+            let work = {
+              id: id,
+              title: title,
+              subtitle: createAt,
+              image: image,
+              website: website,
+            };
+            return <Card work={work} mode="delete" key={id} />;
+          })}
+        </div>
       </div>
-      <div className="grid lg:grid-cols-3 mb-20 gap-10 md:grid-cols-2 sm:grid-cols-1"> 
-      {user.bookmarks.map((bookmark) => {
-        const { id, title, image, createAt, website } = bookmark;
-        let work = {
-          id: id,
-          title: title,
-          subtitle: createAt,
-          image: image,
-          website: website
-        }
-        return (
-          <Card work={work} mode="delete" key={id}/>
-        );
-      })}</div>
-     
-      
     </Layout>
   );
 }
--- pages/works.js
+++ pages/works.js
@@ -77,7 +77,7 @@ export default function Works() {
           initData={initData}
         />
         <div className="py-10">
-          <div className="grid lg:grid-cols-3 gap-10 md:grid-cols-2 sm:grid-cols-1">
+          <div className="lg:grid lg:grid-cols-3 gap-10 md:grid-cols-2 sm:grid-cols-1">
             {works.length > 0 ? (
               works.map((work) => {
                 const { id, links, title, webImage, principalOrFirstMaker } =
